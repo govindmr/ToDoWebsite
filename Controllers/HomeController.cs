81dae89dc9bd7a9f19c64bd174c5460529db9427
GOVIND <GOVIND@DESKTOP-U4VQTI3>
2024-07-05 22:22:19 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDOListModel;
+using AdoRepositories;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.Data.Entity.Migrations;
+
+namespace ToDoWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        ToDoListAdoRepository repository;
+        public HomeController() { 
+
+            repository = new ToDoListAdoRepository();
+        
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ToDOListEntity model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model != null)
+                {
+                    var result = repository.CreateTasks(model);
+                    if (result > 0)
+                    {
+                        ModelState.Clear();
+                        ViewBag.IsSuccess = "Success";
+                    }
+                    else
+                    {
+                        ViewBag.IsSuccess = "Failed";
+                    }
+                }
+            }
+            return View();
+        }
+
+        public ActionResult GetAllTasks()
+        {
+            var result = repository.GetAllTasks();  
+            return View(result);
+        }
+
+        public ActionResult GetTask(int id)
+        {
+            var result = repository.GetTask(id);
+            return View(result);
+        }
+
+        public ActionResult Update(int id)
+        {
+            var result = repository.GetTask(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult Update(ToDOListEntity model)
+        {
+            if (model != null)
+            {
+                var result = repository.Update(model.Id, model);
+            }
+            return RedirectToAction("GetAllTasks");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var result = repository.DeleteTask(id);
+            return RedirectToAction("GetAllTasks");
+        }
+       
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ToDoWebsite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ToDoWebsite")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fcba8156-cb4b-4348-a6f9-f2a2fd75ba4c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
